3a5c6e7c5fc11fbb715980cbfb717013701ad133
manish <manishk.partey@gmail.com>
2022-11-16 15:36:49 +0000

    Add project files.

---
+++ WebAppHostedOnAzureVM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppHostedOnAzureVM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
